ba8196dd4288a7d8fb54416dde3f87a947921362
Kamil <darkmeshas@gmail.com>
2020-07-29 10:57:56 +0200

    Rating data service created

--- rating-data-service/src/main/java/com/example/ratingdataservice/resource/RatingDataResource.java
+++ rating-data-service/src/main/java/com/example/ratingdataservice/resource/RatingDataResource.java
@@ -1,4 +1,16 @@
 package com.example.ratingdataservice.resource;
 
+import com.example.ratingdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
 public class RatingDataResource {
+
+    @RequestMapping("/{movieId")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId,1);
+    }
 }
